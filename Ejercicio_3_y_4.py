0b2859b4f497ac4f3f3c16122d56e01e1227ba16
CarlosCC27 <113622940+CarlosCC27@users.noreply.github.com>
2022-09-29 18:36:57 +0200

    Update Ejercicio_3_y_4.py

--- Ejercicio_3_y_4.py
+++ Ejercicio_3_y_4.py
@@ -1,4 +1,10 @@
 b = int(input("Dame un número"))
 print(b)
 
-
+#Editado Carlos
+edad = int(input("Escribe tu edad"))
+if edad >= 18:
+  print("Eres mayor de edad")
+  
+else:
+  print("Eres menor de edad")
